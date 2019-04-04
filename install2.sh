4d2e4df968c7bbedbf30f9ed2ae1d98974909ddc
layen67 <lkbcontact@gmail.com>
2019-04-04 05:08:25 +0200

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+domain=$1;
+# exit on error
+set -e
+
+# install dependance
+yum install -y curl git zip unzip nano wget;
+wget -qO- https://get.docker.com/ | sh;
+systemctl enable docker;
+systemctl start docker.service;
+curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose;
+chmod +x /usr/local/bin/docker-compose;
+
+yum -y remove postfix;
+
+chmod +x /etc/rc.d/rc.local;
+echo "/var/lib/docker/docker-postal-ubuntu/boot.sh" >> /etc/rc.d/rc.local;
+systemctl enable rc-local;
+
+cd /var/lib/docker;
+git clone https://github.com/layen67/docker-postal-ubuntu.git;
+cd docker-postal-ubuntu/ubuntu;
+chmod +x /var/lib/docker/docker-postal-ubuntu/boot.sh;
+docker-compose up -d;
