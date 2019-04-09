0d045685dfd6e481f602d347cae80af25d71b3d9
layen67 <lkbcontact@gmail.com>
2019-04-09 03:38:14 +0200

    Update postaltls.sh

--- postaltls.sh
+++ postaltls.sh
@@ -106,11 +106,12 @@ echo '' | sudo tee -a /opt/postal/config/postal.yml;
 echo 'smtp_server:' | sudo tee -a /opt/postal/config/postal.yml;
 echo '  port: 25' | sudo tee -a /opt/postal/config/postal.yml;
 echo '  tls_enabled: true' | sudo tee -a /opt/postal/config/postal.yml;
-echo '  tls_certificate_path: /etc/letsencrypt/live/postal.$1/fullchain.pem' | sudo tee -a /opt/postal/config/postal.yml;
-echo '  tls_private_key_path: /etc/letsencrypt/live/postal.$1/privkey.pem' | sudo tee -a /opt/postal/config/postal.yml;
+echo '  tls_certificate_path: /etc/letsencrypt/live/postal.yourdomain.com/fullchain.pem' | sudo tee -a /opt/postal/config/postal.yml;
+echo '  tls_private_key_path: /etc/letsencrypt/live/postal.yourdomain.com/privkey.pem' | sudo tee -a /opt/postal/config/postal.yml;
 echo '  proxy_protocol: false' | sudo tee -a /opt/postal/config/postal.yml;
 echo '  log_connect: true' | sudo tee -a /opt/postal/config/postal.yml;
 echo '  strip_received_headers: false' | sudo tee -a /opt/postal/config/postal.yml;
+sed -i -e "s/yourdomain.com/$1/g" /opt/postal/config/postal.yml;
 
 #
 # All done
