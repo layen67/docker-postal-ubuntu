7add964983b0e7b71db648ae90196178a6c283b7
layen67 <lkbcontact@gmail.com>
2019-11-21 00:48:04 +0100

    Update postalhttpsassassin16.04.sh

--- postalhttpsassassin16.04.sh
+++ postalhttpsassassin16.04.sh
@@ -28,6 +28,8 @@ apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74C
 add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mirrors.coreix.net/mariadb/repo/10.1/ubuntu xenial main';
 curl -sL https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | apt-key add -;
 add-apt-repository 'deb http://www.rabbitmq.com/debian/ testing main';
+apt-add-repository ppa:brightbox/ruby-ng -y
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 apt update;
 export DEBIAN_FRONTEND=noninteractive;
 apt install -y ruby2.3 ruby2.3-dev build-essential mariadb-server libmysqlclient-dev rabbitmq-server nodejs git nginx wget nano;
