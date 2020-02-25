00e7e4cc20d445672bf045a1fd743811b20371b9
layen67 <lkbcontact@gmail.com>
2020-02-26 00:30:27 +0100

    Update khaliddockerpostal.sh

--- khaliddockerpostal.sh
+++ khaliddockerpostal.sh
@@ -259,14 +259,15 @@ networks:
 "> /var/lib/docker/wordpress/docker-compose.yml;
 
 cd /var/lib/docker/wordpress
-docker-compose up -d;
 
-sed -i -r "s/.*tls_certificate_path.*/    tls_certificate_path: \/var\/lib\/docker\/wordpress\/ssl_certs\/postal.$1\/production\/signed.crt;/g" /opt/postal/config/postal.yml
-sed -i -r "s/.*tls_private_key_path.*/    tls_private_key_path: \/var\/lib\/docker\/wordpress\/ssl_certs\/postal.$1\/production\/domain.key;/g" /opt/postal/config/postal.yml
+sed -i -r "s/.*tls_certificate_path.*/    tls_certificate_path: \/var\/lib\/docker\/wordpress\/ssl_certs\/postal.$1\/production\/signed.crt/g" /opt/postal/config/postal.yml;
+sed -i -r "s/.*tls_private_key_path.*/    tls_private_key_path: \/var\/lib\/docker\/wordpress\/ssl_certs\/postal.$1\/production\/domain.key/g" /opt/postal/config/postal.yml;
 sed -i -r "s/.*postal.cert.*/   ssl_certificate          \/var\/lib\/docker\/wordpress\/ssl_certs\/postal.$1\/production\/signed.crt;/g" /etc/nginx/sites-available/default;
 sed -i -r "s/.*postal.key.*/    ssl_certificate_key      \/var\/lib\/docker\/wordpress\/ssl_certs\/postal.$1\/production\/domain.key;/g" /etc/nginx/sites-available/default;
 
-service nginx reload
+docker-compose up -d;
+service nginx restart
+service postal restart
 postal make-user;
 reboot
 #
