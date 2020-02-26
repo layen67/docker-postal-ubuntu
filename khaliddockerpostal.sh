00d03a8bf3a57faedf0511bebe05d915f2f2d2a5
layen67 <lkbcontact@gmail.com>
2020-02-26 02:16:52 +0100

    Update khaliddockerpostal.sh

--- khaliddockerpostal.sh
+++ khaliddockerpostal.sh
@@ -259,8 +259,8 @@ sed -i -r "s/.*postal.cert.*/    ssl_certificate          \/var\/lib\/docker\/wo
 sed -i -r "s/.*postal.key.*/    ssl_certificate_key      \/var\/lib\/docker\/wordpress\/ssl_certs\/postal.$1\/production\/domain.key;/g" /etc/nginx/sites-available/default;
 
 docker-compose up -d;
-service nginx restart
-service postal restart
+sleep 5
+service postal restart;
 postal make-user;
 reboot
 #
